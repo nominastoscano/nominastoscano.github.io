d5175e070ab715defc00d0f457a0ee58fa6f4b07
biotoscano <biotoscano@gmail.com>
2023-01-13 14:17:27 +0100

    TEST

---
+++ script.js
@@ -0,0 +1,26 @@
+function send() {
+    const DNI = document.getElementById('DNI').value;
+
+
+    google.script.url.getLocation(function (location) {
+        const key = location.parameters.key[0];
+        userKey = {
+            key: key,
+            DNI: DNI
+        }
+        console.log(userKey);
+
+        google.script.run
+            .withSuccessHandler(openNomina)
+            .decodeS(userKey)
+    })
+
+}
+
+function openNomina(nominaURL) {
+    console.log(nominaURL);
+
+    // window.location.replace(nominaURL);
+    // window.location.href = nominaURL;
+    window.open(nominaURL, '_blank');
+}
