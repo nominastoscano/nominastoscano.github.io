9bed7f06bec25898ba8f58ae692fb2d320576d72
biotoscano <biotoscano@gmail.com>
2023-01-16 09:51:02 +0100

    adding fetch

--- script.js
+++ script.js
@@ -1,18 +1,77 @@
 function send() {
+
+    console.log('hola');
+
     const DNI = document.getElementById('DNI').value;
 
-    // google.script.url.getLocation(function (location) {
-    //     const key = location.parameters.key[0];
-    //     userKey = {
-    //         key: key,
-    //         DNI: DNI
-    //     }
-    //     console.log(userKey);
+    const url_string = location.href;
+    const url = new URL(url_string);
+    const test = url.searchParams.get("key");
+    console.log(test, DNI);
 
-    //     google.script.run
-    //         .withSuccessHandler(openNomina)
-    //         .decodeS(userKey)
-    // })
+}
+
+function dbUrl() {
+    return 'https://script.google.com/macros/s/AKfycbzPNUO9tVWkQiu0606kiurqVapQwFx9XYcVt9EpINY/dev';
+}
+
+function doLogin(e) {
+
+    e.preventDefault();
+
+    console.log('BUTTON CLICKED');
+
+    let data = Object.fromEntries(new FormData(document.getElementById('userpass')).entries());
+    // console.log(data);
+    const param0 = `${Object.keys(data)[0]}=${Object.values(data)[0]}`;
+    const param1 = `${Object.keys(data)[1]}=${Object.values(data)[1]}`;
+    const params = `?${param0}&${param1}`;
+
+    let url = dbUrl();
+    url += params;
+    // console.log(params)
+
+    fetch(url)
+        .then(r => r.json())
+        .then(r => {
+            console.log("Promise solved");
+            // console.log(r.tempID);
+            // console.log(r.status);
+            document.getElementById('fetchResponse').textContent = r.status;
+            if (r.status === 'logging') {
+                window.location.href = `/home.html?tempID=${r.tempID}`;
+            }
+        })
+
+    document.getElementById('userpass').reset();
+}
+
+async function afterLoad() {
+
+    let url = dbUrl();
+
+    const queryString = window.location.search;
+    // console.log(queryString);
+    // const tempID = new URLSearchParams(window.location.search).get('tempID');
+    url += queryString
+
+    // console.log(tempID);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            console.log('data', data.userDB);
+
+            // addData(data.userDB);
+
+            manageHeader(data.userDB.userPersonal)
+
+            document.getElementById("beautifiedV").innerHTML = JSON.stringify(data.userDB.registroVacaciones, undefined, 2);
+
+
+        })
+        .catch(error => console.log(error));
 
 }
 
